792dc6853d705f7d7ed1da9cf64e078530524c62
dondycles <johnrod.dondoyano@gmail.com>
2024-02-24 00:05:41 +0800

    modified:   components/nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,11 +5,17 @@ import { Button } from "./ui/button";
 import { usePathname, useRouter } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
 import { GrReturn } from "react-icons/gr";
+import { useEffect } from "react";
 export default function Nav() {
   const shuffleMode = useShuffleMode();
   const currentModuleScore = useScore();
   const route = useRouter();
   const pathname = usePathname();
+
+  useEffect(() => {
+    currentModuleScore.setScore(0);
+  }, [pathname]);
+
   return (
     <header className="sticky top-0 left-0 w-full flex flex-row gap-4 justify-between  p-4 sm:px-8 md:px-32 lg:px-64 xl:px-80 border-b-border border-b-[1px] backdrop-blur-sm items-center">
       {/* <div className="flex gap-4">
