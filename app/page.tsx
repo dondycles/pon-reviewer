7bcd38803d1cec01c7adda8857d2d5efda720813
dondycles <johnrod.dondoyano@gmail.com>
2024-02-24 13:39:06 +0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,50 +8,53 @@ import Link from "next/link";
 export default function Home() {
   const modules = [
     {
-      name: "Ibong Tiririt 1",
+      name: "Module 1",
       link: "/module/m1",
     },
     {
-      name: "Ibong Tiririt 2",
+      name: "Module 2",
       link: "/module/m2",
     },
     {
-      name: "Ibong Tiririt 3",
+      name: "Module 3",
       link: "/module/m3",
     },
     {
-      name: "Ibong Tiririt 4",
+      name: "Module 4",
       link: "/module/m4",
     },
     {
-      name: "Ibong Tiririt 5",
+      name: "Module 5",
       link: "/module/m5",
     },
     {
-      name: "Ibong Tiririt 6",
+      name: "Module 6",
       link: "/module/m6",
     },
     {
-      name: "Ibong Tiririt 7",
+      name: "Module 7",
       link: "/module/m7",
     },
   ];
 
   return (
-    <main className="h-full w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid p-4 sm:px-8 md:px-32 lg:px-64 xl:px-80 gap-4 overflow-auto">
-      {modules.map((module) => {
-        return (
-          <Link href={module.link}>
-            <Card className="text-center ">
-              <CardHeader>
-                <CardTitle className="text-lg text-primary">
-                  {module.name}
-                </CardTitle>
-              </CardHeader>
-            </Card>
-          </Link>
-        );
-      })}
+    <main className="h-full w-full p-4  sm:px-8 md:px-32 lg:px-64 xl:px-80 overflow-auto">
+      <p className="text-2xl text-primary font-bold text-center">Ibong Tirit</p>
+      <div className="mt-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid gap-4 ">
+        {modules.map((module) => {
+          return (
+            <Link href={module.link}>
+              <Card className="text-center hover:bg-primary/15">
+                <CardHeader>
+                  <CardTitle className="text-lg text-primary">
+                    {module.name}
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            </Link>
+          );
+        })}
+      </div>
     </main>
   );
 }
